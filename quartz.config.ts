3abf100b3f83d98ddd2683da691fac09879c8e99
Heetaek Woo <hiiteq@gmail.com>
2025-08-29 10:05:55 +0900

    fix: Correct baseUrl for GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "woohannah.com",
+    baseUrl: "hiteq.github.io/woohannahdotcom",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
